11fbef0a741ba935455a22e1f49959a5abd5cfb3
Alif Naufal <alifnaufalyasin@gmail.com>
2021-09-27 23:54:54 +0700

    add controller

---
+++ controller/mahasiswa.go
@@ -0,0 +1,148 @@
+package controller
+
+import (
+	"fmt"
+	"gits-echo-boilerplate/database"
+	"gits-echo-boilerplate/models"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/oklog/ulid"
+)
+
+func CreateMahasiswa(c echo.Context) error {
+	m := new(models.Mahasiswa)
+	if err := c.Bind(m); err != nil {
+		return (models.JSONResponse{
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		}).Response(c)
+	}
+
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+	m.ID = ulid.MustNew(ulid.Timestamp(time.Now()), entropy).String()
+	m.CreatedAt = time.Now()
+	m.UpdatedAt = time.Now()
+
+	if err := m.Validate(); err.Code > 0 {
+		fmt.Println("sini", err.Message)
+		return (models.JSONResponse{
+			Code:    err.Code,
+			Message: err.Message,
+		}).Response(c)
+	}
+
+	err := database.CreateMahasiswa(m)
+	if err.Code > 0 {
+		return (models.JSONResponse{
+			Code:    err.Code,
+			Message: "Gagal menambahkan data mahasiswa",
+		}).Response(c)
+	}
+	resp := models.JSONResponseData{
+		Code:    http.StatusCreated,
+		Data:    m,
+		Message: "Data Created",
+	}
+	return resp.Response(c)
+
+}
+
+func GetMahasiswaByID(c echo.Context) error {
+	id := c.Param("id")
+
+	m, err := database.GetMahasiswaByID(id)
+	if err.Code > 0 {
+		return (models.JSONResponse{
+			Code:    err.Code,
+			Message: err.Message,
+		}).Response(c)
+	}
+
+	return (models.JSONResponseData{
+		Code:    http.StatusOK,
+		Data:    m,
+		Message: "Success",
+	}).Response(c)
+}
+
+func GetAllMahasiswa(c echo.Context) error {
+	kelas := c.QueryParam("kelas")
+	umur := c.QueryParam("umur")
+	i, _ := strconv.ParseInt(umur, 10, 64)
+	m, err := database.GetAllMahasiswa(database.FilterSearch{
+		Umur:  i,
+		Kelas: kelas,
+	})
+	if err.Code > 0 {
+		return (models.JSONResponse{
+			Code:    err.Code,
+			Message: err.Message,
+		}).Response(c)
+	}
+
+	return (models.JSONResponseData{
+		Code:    http.StatusOK,
+		Data:    m,
+		Message: "Success",
+	}).Response(c)
+
+}
+
+func UpdateMahasiswa(c echo.Context) error {
+	id := c.Param("id")
+
+	m := new(models.Mahasiswa)
+	if err := c.Bind(m); err != nil {
+		return (models.JSONResponse{
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		}).Response(c)
+	}
+	m.ID = id
+	m.UpdatedAt = time.Now()
+
+	if err := m.Validate(); err.Code > 0 {
+		fmt.Println("sini", err.Message)
+		return (models.JSONResponse{
+			Code:    err.Code,
+			Message: err.Message,
+		}).Response(c)
+	}
+
+	_, err := database.UpdateMahasiswa(m)
+	if err.Code > 0 {
+		return (models.JSONResponse{
+			Code:    err.Code,
+			Message: err.Message,
+		}).Response(c)
+	}
+	resp := models.JSONResponseData{
+		Code:    http.StatusOK,
+		Data:    m,
+		Message: "Success",
+	}
+	return resp.Response(c)
+}
+
+func DeleteMahasiswa(c echo.Context) error {
+	id := c.Param("id")
+
+	_, err := database.DeleteMahasiswa(id)
+
+	if err.Code > 0 {
+		return (models.JSONResponse{
+			Code:    err.Code,
+			Message: err.Message,
+		}).Response(c)
+	}
+
+	resp := models.JSONResponse{
+		Code:    http.StatusOK,
+		Message: "Success",
+	}
+	return resp.Response(c)
+}
